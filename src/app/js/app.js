0e5407a0881641c10822d00d2ef04516f805dfba
Rand Seay <rand.seay@gmail.com>
2015-11-23 22:13:00 -0600

    Finish competition-based leaderboard.

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,8 +1,9 @@
 'use strict';
 
+var Competitions = require('./components/CompetitionsTable');
 var Leaderboard = require('./components/Leaderboard');
 var React = require('react');
 
 React.render(
-    <Leaderboard url={'http://leaderboard-project.azurewebsites.net/api/competition/dogs-vs-cats/leaderboard'}/>, document.getElementById('Leaderboard')
+    <Leaderboard url={'http://leaderboard-project.azurewebsites.net/api/competition/dogs-vs-cats/leaderboard'} activeUser={'DaggerFS'}/>, document.getElementById('Leaderboard')
 );
